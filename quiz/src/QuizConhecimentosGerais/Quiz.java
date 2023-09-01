81795ac79ed047d5111e339cf939e646026e886f
Kauã Rasera <r.asera@outlook.com>
2023-09-01 18:25:22 -0300

    v2 etapa 1 quiz atualizada
    Atualizando total de questões e refatorando

--- quiz/src/QuizConhecimentosGerais/Quiz.java
+++ quiz/src/QuizConhecimentosGerais/Quiz.java
@@ -19,7 +19,7 @@ import java.util.Scanner;
 public class Quiz {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
-        int i, totResp = 4;
+        int i, totResp = 10;
         String gab[] = new String[totResp];
         String resp;
 
@@ -27,41 +27,55 @@ public class Quiz {
         System.out.println("ETAPA 1 - Cadastro de Respostas");
         System.out.println("----------------------------------");
         for (i = 0; i < totResp; i++) {
-            System.out.print("Questão " + i + ": ");
+            System.out.print("Questão " + (i + 1) + ": ");
             gab[i] = leia.nextLine();
-
-            // Vai ler cada pergunta e receber uma
-            Scanner ler = new Scanner(System.in);
-            int totQuest = 4;
-            String[] quest[] = {
-                    { "Qual a classe dos golginhos?", "A) Peixe ", "B) Anfibio", "C) Mamiferio" },
-                    { "Qual é a capital da França?", "A) Londres", "B) Madri", "C) Berlim" },
-                    { "Qual é o maior planeta do Sistema Solar?", "A) Terra", "B) Marte", "C) Júpiter" },
-                    { "Qual é o maior mamífero terrestre?", "A) Elefante Africano", "B) Girafa", "C) Rinoceronte" }
-            };
-
-            int respCorreta = 0;
-            int respErrada = 0;
-
-            System.out.println("QUIZ DE CONHECIMENTOS GERAIS: ");
-            for (i = 0; i < totQuest; i++) {
-                System.out.println("Pergunta " + i + ":");
-                System.out.println(quest[i][0]);
-                for (int c = 1; c < totQuest; c++) {
-                    System.out.println(quest[i][c]);
-                }
-                System.out.println("SELECIONE A RESPOSTA CORRETA");
-                System.out.println("a | b | c ");
-                quest[i][0] = ler.nextLine();
-            }
-
-             //Verificando se a resposta do usuario bate com o gabarito do quiz
-            boolean condicaoRespCorreta = (gab[i] ==  quest[i][0] );
-            if (condicaoRespCorreta) {
-                respCorreta = respCorreta + 1;
-            } else {
-                respErrada = respErrada + 1;
-            }
         }
+        // Vai ler cada pergunta e receber uma
+        System.out.println("----------------------------------");
+        System.out.println(" QUIZ CONHECIMENTOS GERAIS");
+        Scanner ler = new Scanner(System.in);
+        int totQuest = 10;
+        String[] quest[] = {
+                { "Qual a classe dos golginhos?", "A) Peixe ", "B) Anfibio", "C) Mamiferio", "D) Aves" },
+                { "Qual é a capital da França?", "A) Londres", "B) Madri", "C) Berlim", "D) Paris" },
+                { "Qual é o maior planeta do Sistema Solar?", "A) Terra", "B) Marte", "C) Júpiter", "D) Vênus" },
+                { "Qual é o maior mamífero terrestre?", "A) Elefante Africano", "B) Girafa", "C) Rinoceronte",
+                        "D) Baleia Azul" },
+                { "Quem foi o primeiro ser humano a pisar na Lua?", "A) Neil Armstrong ", "B) Yuri Gagarin",
+                        "C) Buzz Aldrin", "D) John Glenn" },
+                { "Qual é a pintura mais famosa de Leonardo da Vinci?", "A) A Última Ceia", "B) A Noite Estrelada",
+                        "C) Mona Lisa", "D) Guernica" },
+                { "Qual é o maior oceano do mundo?", "A) Atlântico", "B) Ìndico", "C) Ártico", "D) Pacífico" },
+                { "Qual é amaior ilha do mundo", "A) Havaí", "B) Japão", "C) Madagascar", "D) Groelândia" },
+                { "Qual é o rio mais longo do mundo?", "A) Rio Nilo", "B) Rio Amazonas", "C) Rio Mississipi",
+                        "D) Rio Yangtzé" },
+                { "Quem escreveu a obra Dom Quixote?", "A) Charles Dickens", "B) Miguel de Cervantes", "C) Leo Tolstoy",
+                        "D)Jane Austen" }
+        };
+
+        int respCorreta = 0;
+        int respErrada = 0;
+
+        System.out.println("QUIZ DE CONHECIMENTOS GERAIS: ");
+        for (i = 0; i < totQuest; i++) {
+            System.out.println("Pergunta " + (i + 1) + ":");
+            System.out.println(quest[i][0]);
+            for (int c = 1; c < totQuest; c++) {
+                System.out.println(quest[i][c]);
+            }
+            System.out.println("SELECIONE A RESPOSTA CORRETA");
+            System.out.println("a | b | c | d ");
+            quest[i][0] = ler.nextLine();
+        }
+
+        // Verificando se a resposta do usuario bate com o gabarito do quiz
+        boolean condicaoRespCorreta = (gab[i] == quest[i][0]);
+        if (condicaoRespCorreta) {
+            respCorreta = respCorreta + 1;
+        } else {
+            respErrada = respErrada + 1;
+        }
+
+        // Resumo quz
     }
 }
