1f12438988349508d788ea61be9eca83e5318daf
Kauã Rasera <r.asera@outlook.com>
2023-09-02 19:16:42 -0300

    Primeira etapa atualizada e corrigida falta o resumo do quiz

--- quiz/src/QuizConhecimentosGerais/Quiz.java
+++ quiz/src/QuizConhecimentosGerais/Quiz.java
@@ -7,70 +7,70 @@ public class Quiz {
         Scanner leia = new Scanner(System.in);
         int totResp = 10;
         int i;
-        String gabAnswers[] = { "c", "d", "c", "d", "a", "c", "d", "d", "a", "b" };
-
-        // Gabarito para comparar com a resposta do usuario
-        System.out.println("ETAPA 1 - Cadastro de Respostas");
-        System.out.println("----------------------------------");
-        for (i = 0; i < totResp; i++) {
-            System.out.print("Questão " + (i + 1) + ": ");
-            System.out.println(gabAnswers[i]);
-        }
-
-        limpaTela();
-
-        // Vai ler cada pergunta e receber uma respsta
-        System.out.println("----------------------------------");
-        System.out.println(" QUIZ CONHECIMENTOS GERAIS");
-
         String resposta;
         int totPerguntas = 10;
         int respCorreta = 0;
         int respErrada = 0;
+        String gabRespostas[] = { "c", "d", "c", "d", "a", "c", "d", "d", "a", "b" };
         String respostaUsuario[] = new String[totPerguntas];
-        String perguntas[] = {
-                "Qual a classe dos golginhos?",
-                "Qual é a capital da França?",
-                "Qual é o maior planeta do Sistema Solar?",
-                "Qual é o maior mamífero terrestre?",
-                "Quem foi o primeiro ser humano a pisar na Lua?",
-                "Qual é a pintura mais famosa de Leonardo da Vinci?",
-                "Qual é o maior oceano do mundo?",
-                "Qual é a maior ilha do mundo",
-                "Qual é o rio mais longo do mundo?", "D) Rio Yangtzé",
-                "Quem escreveu a obra Dom Quixote?"
+
+        String[] perguntas[] = {
+                { "Qual a classe dos golginhos?", "A) Peixe ", " B) Anfibio", " C) Mamiferio", " D) Aves" },
+                { "Qual é a capital da França?", "A) Londres", " B) Madri", " C) Berlim", "D) Paris" },
+                { "Qual é o maior planeta do Sistema Solar?", " A) Terra", "B) Marte", " C) Júpiter", "D) Vênus" },
+                { "Qual é o maior mamífero terrestre?", " A) Elefante Africano", " B) Girafa", " C) Rinoceronte",
+                        " D) Baleia Azul" },
+                { "Quem foi o primeiro ser humano a pisar na Lua?", " A) Neil Armstrong ", "B) Yuri Gagarin",
+                        " C) Buzz Aldrin", " D) John Glenn" },
+                { "Qual é a pintura mais famosa de Leonardo da Vinci?", " A) A Última Ceia", " B) A Noite Estrelada",
+                        " C) Monalisa", " D) Guernica" },
+                { "Qual é o maior oceano do mundo?", " A) Atlântico", " B) Ìndico", " C) Ártico", " D) Pacífico" },
+                { "Qual é a maior ilha do mundo", " A) Havaí", " B) Japão", " C) Madagascar", " D) Groenlândia" },
+                { "Qual é o rio mais longo do mundo?", " A) Rio Nilo", " B) Rio Amazonas", " C) Rio Mississipi",
+                        " D) Rio Yangtzé" },
+                { "Quem escreveu a obra Dom Quixote?", " A) Charles Dickens", " B) Miguel de Cervantes",
+                        " C) Leo Tolstoy",
+                        " D)Jane Austen" }
         };
 
-        String alternativas[] = { "| a |", " b |", " c |", " d |" };
+        // Vai ler cada pergunta e receber uma respsta
+        System.out.println("----------------------------------");
+        System.out.println(" QUIZ CONHECIMENTOS GERAIS");
+        System.out.println("----------------------------------");
 
         for (i = 0; i < totPerguntas; i++) {
-            System.out.println("Pergunta " + (i + 1) + ": " + perguntas[i]);
-            for (int c = 0; c < 4; c++) {
-                System.out.print(alternativas[c]);
+            System.out.println("Pergunta " + (i + 1) + ": " + perguntas[i][0]);
+            for (int c = 1; c < 5; c++) {
+                System.out.print(perguntas[i][c]);
             }
+
             System.out.println();
             System.out.println("-----------------------------------");
-            System.out.print("DIGITE A ALTERNATIVA CORRETA: ");
-            resposta = leia.nextLine();
+            System.out.print("DIGITE A ALTERNATIVA CORRETA (A) (B) (C) (D): \n -> ");
+            resposta = leia.nextLine().toLowerCase();
+
             respostaUsuario[i] = resposta;
-            System.out.println();
-        }
-        // Verificando se a resposta do usuario bate com o gabarito do quiz
-        boolean condicaoRespCorreta = (respostaUsuario[i] != gabAnswers[i]);
 
-        for (i = 0; i < totPerguntas; i++) {
+            // Verificando se a resposta do usuario bate com o gabarito do quiz
+            //boolean condicaoRespCorreta = (respostaUsuario[i] == gabRespostas[i]);
 
-            if (condicaoRespCorreta) {
-                respCorreta = respCorreta + 1;
-                System.out.println("Você errou " + respErrada + " de " + totPerguntas + " perguntas.");
-
+            if (respostaUsuario[i].equals(gabRespostas[i])) {
+                respCorreta++;
+                System.out.println("Acertou \n");
             } else {
-
-                System.out.println("Você acertou " + respCorreta + " de " + totPerguntas + " perguntas.");
+                respErrada++;
+                System.out.println("Errou. A resposta correta é: " + gabRespostas[i] + "\n");
             }
         }
 
+        limpaTela();
+
+        System.out.println("=== Resumo do Quiz ===");
+
+
+        System.out.println();
         System.out.println("QUIZ CONCLUIDO, PARABÉNS!!!");
+        System.out.println();
     }
 
     // Método para limpar a tela imprimindo quebras de linha
