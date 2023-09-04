e8bdca0028cf2295146888323bbbc72afab0e4f7
Kauã Rasera <r.asera@outlook.com>
2023-09-04 16:55:16 -0300

    Indentando codigo

--- quiz/src/QuizConhecimentosGerais/Quiz.java
+++ quiz/src/QuizConhecimentosGerais/Quiz.java
@@ -4,36 +4,37 @@ import java.util.Scanner;
 
 public class Quiz {
     public static void main(String[] args) {
-     String op;
-    do {
-        Scanner leia = new Scanner(System.in);
-        int i;
-        String resposta;
-        int totPerguntas = 10;
-        int respCorreta = 0;
-        int respErrada = 0;
-        int porcentagemAcertos = 0;
-        String gabRespostas[] = { "c", "d", "c", "d", "a", "c", "d", "d", "a", "b" };
-        String respostaUsuario[] = new String[totPerguntas];
+        String op;
+        do {
+            Scanner leia = new Scanner(System.in);
+            int i;
+            String resposta;
+            int totPerguntas = 10;
+            int respCorreta = 0;
+            int respErrada = 0;
+            int porcentagemAcertos = 0;
+            String gabRespostas[] = { "c", "d", "c", "d", "a", "c", "d", "d", "a", "b" };
+            String respostaUsuario[] = new String[totPerguntas];
 
-        String[] perguntas[] = {
-                { "Qual a classe dos golginhos?", "A) Peixe ", " B) Anfibio", " C) Mamiferio", " D) Aves" },
-                { "Qual é a capital da França?", "A) Londres", " B) Madri", " C) Berlim", "D) Paris" },
-                { "Qual é o maior planeta do Sistema Solar?", " A) Terra", "B) Marte", " C) Júpiter", "D) Vênus" },
-                { "Qual é o maior mamífero terrestre?", " A) Elefante Africano", " B) Girafa", " C) Rinoceronte",
-                        " D) Baleia Azul" },
-                { "Quem foi o primeiro ser humano a pisar na Lua?", " A) Neil Armstrong ", "B) Yuri Gagarin",
-                        " C) Buzz Aldrin", " D) John Glenn" },
-                { "Qual é a pintura mais famosa de Leonardo da Vinci?", " A) A Última Ceia", " B) A Noite Estrelada",
-                        " C) Monalisa", " D) Guernica" },
-                { "Qual é o maior oceano do mundo?", " A) Atlântico", " B) Ìndico", " C) Ártico", " D) Pacífico" },
-                { "Qual é a maior ilha do mundo", " A) Havaí", " B) Japão", " C) Madagascar", " D) Groenlândia" },
-                { "Qual é o rio mais longo do mundo?", " A) Rio Nilo", " B) Rio Amazonas", " C) Rio Mississipi",
-                        " D) Rio Yangtzé" },
-                { "Quem escreveu a obra Dom Quixote?", " A) Charles Dickens", " B) Miguel de Cervantes",
-                        " C) Leo Tolstoy",
-                        " D)Jane Austen" }
-        };
+            String[] perguntas[] = {
+                    { "Qual a classe dos golginhos?", "A) Peixe ", " B) Anfibio", " C) Mamiferio", " D) Aves" },
+                    { "Qual é a capital da França?", "A) Londres", " B) Madri", " C) Berlim", "D) Paris" },
+                    { "Qual é o maior planeta do Sistema Solar?", " A) Terra", "B) Marte", " C) Júpiter", "D) Vênus" },
+                    { "Qual é o maior mamífero terrestre?", " A) Elefante Africano", " B) Girafa", " C) Rinoceronte",
+                            " D) Baleia Azul" },
+                    { "Quem foi o primeiro ser humano a pisar na Lua?", " A) Neil Armstrong ", "B) Yuri Gagarin",
+                            " C) Buzz Aldrin", " D) John Glenn" },
+                    { "Qual é a pintura mais famosa de Leonardo da Vinci?", " A) A Última Ceia",
+                            " B) A Noite Estrelada",
+                            " C) Monalisa", " D) Guernica" },
+                    { "Qual é o maior oceano do mundo?", " A) Atlântico", " B) Ìndico", " C) Ártico", " D) Pacífico" },
+                    { "Qual é a maior ilha do mundo", " A) Havaí", " B) Japão", " C) Madagascar", " D) Groenlândia" },
+                    { "Qual é o rio mais longo do mundo?", " A) Rio Nilo", " B) Rio Amazonas", " C) Rio Mississipi",
+                            " D) Rio Yangtzé" },
+                    { "Quem escreveu a obra Dom Quixote?", " A) Charles Dickens", " B) Miguel de Cervantes",
+                            " C) Leo Tolstoy",
+                            " D)Jane Austen" }
+            };
             // Vai ler cada pergunta e receber uma respsta
             System.out.println("----------------------------------");
             System.out.println(" QUIZ CONHECIMENTOS GERAIS");
