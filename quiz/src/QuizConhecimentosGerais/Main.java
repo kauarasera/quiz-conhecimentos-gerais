09af6b6de0e9188be7fe0ecd8f8dcc88f9e93fbb
Kauã Rasera <kauarasera@gmail.com>
2023-10-16 17:11:25 -0300

    Padronizando em POO

---
+++ quiz/src/QuizConhecimentosGerais/Main.java
@@ -0,0 +1,8 @@
+package QuizConhecimentosGerais;
+
+public class Main {
+    public static void main(String[] args) {
+        Quiz quiz = new Quiz(10);
+        quiz.executarQuiz();
+    }
+}
--- quiz/src/QuizConhecimentosGerais/Quiz.java
+++ quiz/src/QuizConhecimentosGerais/Quiz.java
@@ -3,42 +3,19 @@ package QuizConhecimentosGerais;
 import java.util.Scanner;
 
 public class Quiz {
-    public static void main(String[] args) {
-        Quiz quiz = new Quiz();
-        quiz.executarQuiz();
-    }
 
-    // Função para imprimir uma questão
-    private void imprimirQuestao(String[] pergunta) {
-        System.out.println("Pergunta: " + pergunta[0]);
-        for (int i = 1; i < pergunta.length; i++) {
-            System.out.println(pergunta[i]);
-        }
-    }
+    private String[] gabRespostas;
+    private String[] respostaUsuario;
+    private int respCorreta;
+    private int respErrada;
+    private int totalPerguntas;
 
-    // procedimento para imprimir o resumo do quiz
-    private void imprimirResumo(String[] gabRespostas,
-                                String[] respostaUsuario,
-                                int respCorreta,
-                                int respErrada,
-                                int totPerguntas) {
-        System.out.println("============== Resumo do Quiz ==============");
-        System.out.println(" Pergunta  |  Resposta escolhida  | Resposta correta  | Correção ");
-        for (int i = 0; i < totPerguntas; i++) {
-            String numeroFormatado = String.format("%02d", i + 1);
-            String correcao = respostaUsuario[i].equals(gabRespostas[i]) ? "v" : "X";
-            System.out.println("     " + numeroFormatado + "     |          " + respostaUsuario[i] + "            |         "
-                    + gabRespostas[i] + "          |     " + correcao);
-        }
-        int porcentagemAcertos = (respCorreta * 100) / totPerguntas;
-        System.out.println();
-        System.out.println("Você acertou " + respCorreta + " e errou " + respErrada + " de " + totPerguntas + " perguntas.");
-
-        if (porcentagemAcertos >= 60) {
-            System.out.println("Você passou com " + porcentagemAcertos + "% de acertos");
-        } else {
-            System.out.println("Você NÃO passou com somente " + porcentagemAcertos + "% de acertos");
-        }
+    public Quiz( int totalPerguntas) {
+        this.totalPerguntas = totalPerguntas;
+        this.respCorreta = 0;
+        this.respErrada = 0;
+        this.gabRespostas = new String[]{"c", "d", "c", "d", "a", "c", "d", "d", "a", "b"};;
+        this.respostaUsuario = new String[totalPerguntas];
     }
 
     // Procedimento para executar o quiz
@@ -47,37 +24,13 @@ public class Quiz {
         String op;
 
         do {
-            int totPerguntas = 10;
-            int respCorreta = 0;
-            int respErrada = 0;
-            String[] gabRespostas = {"c", "d", "c", "d", "a", "c", "d", "d", "a", "b"};
-            String[] respostaUsuario = new String[totPerguntas];
-            String[][] perguntas = {
-                    {"Qual a classe dos golfinhos?", " A) Peixe ", " B) Anfíbio", " C) Mamífero", " D) Aves"},
-                    {"Qual é a capital da França?", " A) Londres", " B) Madri", " C) Berlim", " D) Paris"},
-                    {"Qual é o maior planeta do Sistema Solar?", " A) Terra", " B) Marte", " C) Júpiter", " D) Vênus"},
-                    {"Qual é o maior mamífero terrestre?", " A) Elefante Africano", " B) Girafa", " C) Rinoceronte",
-                            " D) Baleia Azul"},
-                    {"Quem foi o primeiro ser humano a pisar na Lua?", " A) Neil Armstrong ", " B) Yuri Gagarin",
-                            " C) Buzz Aldrin", " D) John Glenn"},
-                    {"Qual é a pintura mais famosa de Leonardo da Vinci?", " A) A Última Ceia",
-                            " B) A Noite Estrelada",
-                            " C) Monalisa", " D) Guernica"},
-                    {"Qual é o maior oceano do mundo?", " A) Atlântico", " B) Índico", " C) Ártico", " D) Pacífico"},
-                    {"Qual é a maior ilha do mundo?", " A) Havaí", " B) Japão", " C) Madagascar", " D) Groenlândia"},
-                    {"Qual é o rio mais longo do mundo?", " A) Rio Nilo", " B) Rio Amazonas", " C) Rio Mississipi",
-                            " D) Rio Yangtzé"},
-                    {"Quem escreveu a obra Dom Quixote?", " A) Charles Dickens", " B) Miguel de Cervantes",
-                            " C) Leo Tolstoy",
-                            " D) Jane Austen"}
-            };
-
+            limpaTela();
             System.out.println("----------------------------------");
             System.out.println(" QUIZ CONHECIMENTOS GERAIS");
             System.out.println("----------------------------------");
 
-            for (int i = 0; i < totPerguntas; i++) {
-                imprimirQuestao(perguntas[i]);
+            for (int i = 0; i < totalPerguntas; i++) {
+                imprimirQuestao(Perguntas.perguntas[i]);
                 System.out.print("DIGITE A ALTERNATIVA CORRETA (A) (B) (C) (D): \n -> ");
                 String resposta = leia.nextLine().toLowerCase();
                 respostaUsuario[i] = resposta;
@@ -95,7 +48,7 @@ public class Quiz {
             limpaTela();
 
             //chamando procedimento para imprimir resumo
-            imprimirResumo(gabRespostas, respostaUsuario, respCorreta, respErrada, totPerguntas);
+            imprimirResumo();
 
             //Caso usuario não tenha obtido mais que 60% de acertos ele tem a opção de refazer o quiz.
             System.out.print("Deseja tentar outra vez? [S/N]: ");
@@ -109,6 +62,34 @@ public class Quiz {
         System.out.println("QUIZ CONCLUÍDO, ATÉ BREVE!!!");
         System.out.println();
     }
+    // Função para imprimir uma questão
+    private void imprimirQuestao(String[] pergunta) {
+        System.out.println("Pergunta: " + pergunta[0]);
+        for (int i = 1; i < pergunta.length; i++) {
+            System.out.println(pergunta[i]);
+        }
+    }
+
+    // procedimento para imprimir o resumo do quiz
+    private void imprimirResumo() {
+        System.out.println("============== Resumo do Quiz ==============");
+        System.out.println(" Pergunta  |  Resposta escolhida  | Resposta correta  | Correção ");
+        for (int i = 0; i < totalPerguntas; i++) {
+            String numeroFormatado = String.format("%02d", i + 1);
+            String correcao = respostaUsuario[i].equals(gabRespostas[i]) ? "v" : "X";
+            System.out.println("     " + numeroFormatado + "     |          " + respostaUsuario[i] + "            |         "
+                    + gabRespostas[i] + "          |     " + correcao);
+        }
+        int porcentagemAcertos = (respCorreta * 100) / totalPerguntas;
+        System.out.println();
+        System.out.println("Você acertou " + respCorreta + " e errou " + respErrada + " de " + totalPerguntas + " perguntas.");
+
+        if (porcentagemAcertos >= 60) {
+            System.out.println("Você passou com " + porcentagemAcertos + "% de acertos");
+        } else {
+            System.out.println("Você NÃO passou com somente " + porcentagemAcertos + "% de acertos");
+        }
+    }
 
     // procedimento para limpar a tela imprimindo quebras de linha
     private static void limpaTela() {
@@ -117,4 +98,3 @@ public class Quiz {
         }
     }
 }
-
